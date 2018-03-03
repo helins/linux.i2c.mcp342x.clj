dba415bb63b9d440c1b0e053889486036aebec6b
Adam Helinski <helinski.adam@outlook.com>
2018-03-03 13:32:33 +0100

    Rename project and propose new API

--- project.clj
+++ project.clj
@@ -1,19 +1,21 @@
-(defproject dvlopt/mcp342x
-            "0.0.0-alpha2"
+(defproject dvlopt/i2c.mcp342x
+            "0.0.0"
 
-  :description "A/D with MCP342x converters via I²C"
-  :url         "https://github.com/dvlopt/mcp342x.clj"
-  :license     {:name "Eclipse Public License"
-                :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles    {:dev {:source-paths ["dev"]
-                      :main         user
-                      :dependencies [[org.clojure/clojure    "1.9.0"]
-                                     [org.clojure/test.check "0.10.0-alpha2"]
-                                     [criterium              "0.4.4"]
-                                     [dvlopt/icare           "0.0.0-alpha1"]]
-                      :plugins      [[venantius/ultra "0.5.1"]
-                                     [lein-codox      "0.10.3"]]
-                      :codox        {:output-path  "doc/auto"
-                                     :source-paths ["src"]}
-                      :repl-options {:timeout 180000}
-                      :global-vars  {*warn-on-reflection* true}}})
+  :description  "Talk to the MCP342x family of ADC via I2C"
+  :url          "https://github.com/dvlopt/i2c.mcp342x"
+  :license      {:name "Eclipse Public License"
+                 :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[dvlopt/i2c  "0.0.0"]
+                 [dvlopt/void "0.0.0"]]
+  :profiles     {:dev {:source-paths ["dev"]
+                       :main         user
+                       :dependencies [[criterium              "0.4.4"]
+                                      [dvlopt/i2c           "0.0.0"]
+                                      [org.clojure/clojure    "1.9.0"]
+                                      [org.clojure/test.check "0.10.0-alpha2"]]
+                       :plugins      [[lein-codox      "0.10.3"]
+                                      [venantius/ultra "0.5.2"]]
+                       :codox        {:output-path  "doc/auto"
+                                      :source-paths ["src"]}
+                       :repl-options {:timeout 180000}
+                       :global-vars  {*warn-on-reflection* true}}})
